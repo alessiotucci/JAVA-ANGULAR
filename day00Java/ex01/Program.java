91bfe6fedbcd5f302affad272909d0573fac54fa
Alessio Tucci <alessiotucci33@gmail.com>
2024-05-01 16:36:47 +0200

    finished up the first module of java

--- day00Java/ex01/Program.java
+++ day00Java/ex01/Program.java
@@ -1,4 +1,38 @@
+import java.util.Scanner;
 
+public class Program
+{
+	public static void checkPrime(int param)
+	{
+		if (param <= 1)
+		{
+			System.out.println("IllegalArgument");
+		}
+		else
+		{
+			int ret = 1;
+			boolean check = true;
+			int iter = 2;
+			while (iter * iter <= param)
+			{
+				if (param % iter == 0)
+				{
+					check = false;
+					break;
+				}
+				iter++;
+				ret++;
+			}
+			System.out.println(check + " "+ ret);
+		}
+	}
+	public static void main(String[] args)
+	{
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		checkPrime(number);
+	}
+}
 
-
-
+/* Allowed functions :
+Input/Output : System.out, System.err, Scanner(System.in) */
