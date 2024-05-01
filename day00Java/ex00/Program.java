287b9be0f984575b9732c0461ad0b16aec83d1ef
Alessio Tucci <alessiotucci33@gmail.com>
2024-05-01 16:12:31 +0200

    working on java

--- day00Java/ex00/Program.java
+++ day00Java/ex00/Program.java
@@ -1,4 +1,20 @@
-
-
-
-
+public class Program
+{
+	public static int sumDigits(int number)
+	{
+		int sum = 0;
+		while (number != 0)
+		{
+			int digit = number % 10;
+			sum += digit;
+			number /= 10;
+		}
+		return sum;
+	}
+	public static void main(String[] args)
+	{
+		int number = 479598;
+		int sum = sumDigits(number);
+		System.out.println(sum);
+	}
+}
